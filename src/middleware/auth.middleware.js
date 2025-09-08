ea02fa35f4b2261627c55f1a43566f8e1e16908d
Vidyashreesv <vidyashreesv1999@gmail.com>
2025-09-08 22:27:46 +0530

    this is because something is wrong in the code

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-const protectRoute = async (req, resizeBy, next) => {
+const protectRoute = async (req, res, next) => {
   try {
     //get token
     const token = req.header("Authorization").replace("Bearer ", "");
